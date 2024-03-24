3c383fe4ea7301b652558a3e5efb17c8c8ce36c7
Mike Denton <geminimatrixinc@gmail.com>
2024-03-24 12:07:42 -0400

    remove project files

--- tutorial-1-intro-to-react/src/components/Greeter.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Greeter() {
-    return <h2>Hello, React!</h2>
-}
-
-export default Greeter;
--- tutorial-4-user-input-events/beginner/react-input-events-form/src/FormInput.js
+++
@@ -1,6 +0,0 @@
-
-const FormInput = () => {
-
-
-    return <></>
-}
