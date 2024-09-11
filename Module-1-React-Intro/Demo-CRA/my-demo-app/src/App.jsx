9aafccf111e9bc9aeabccefc589b490c4b07c5d5
Mike Denton <geminimatrixinc@gmail.com>
2024-09-11 19:01:46 -0400

    react essentials modules

---
+++ Module-1-React-Intro/Demo-CRA/my-demo-app/src/App.jsx
@@ -0,0 +1,20 @@
+
+import JsxMultipleElements from "./components/2-Component-JSX/3-Jsx-Multiple-Elements";
+import JsxConditionalRender from "./components/2-Component-JSX/4-Jsx-Conditional-Rendering";
+
+function App() {
+
+
+  return (
+        <>
+           hello world
+           <JsxMultipleElements />
+           <JsxConditionalRender />
+        </>     
+  
+  );
+}
+
+
+export default App;
+
